6c8b07cf35251489e55bedbe8eb986fee1d08182
Gizem Sultan Dinc <gizemmdinncc@gmail.com>
2026-02-16 17:50:46 +0300

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe("ilk test", () => {
+
+  it("https://www.edu.goit.global/tr/account/login", () => {
+
+    cy.visit("https://www.edu.goit.global/tr/account/login");
+
+  });
+
+});
