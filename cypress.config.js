8af0b70bed46dfff253ca114b4f5055853aefc62
Gizem Sultan Dinc <gizemmdinncc@gmail.com>
2026-03-04 20:04:17 +0300

    Fix Cypress config for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.js",
   },
 });
-
 
 
