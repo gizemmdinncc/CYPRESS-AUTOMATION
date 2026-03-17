0e30ae9a8ee3bb426f8614da7ccbc1bdbac9417d
Gizem Sultan Dinc <gizemmdinncc@gmail.com>
2026-03-17 16:23:55 +0300

    Restore working Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.js",
     
   },
 });
-
 
